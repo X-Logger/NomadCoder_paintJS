afdb43cf36ace3d2dc173aed122298a6809c2c20
김형찬 <koc0819@gmail.com>
2020-05-16 16:47:35 +0900

    day-2
    선 크기 바꾸기
    색 채우기 (fill)
    색상 바꾸기

--- app.js
+++ app.js
@@ -1,39 +1,87 @@
-const canvas = document.querySelector("#jsCanvas");
+const canvas = document.querySelector('#jsCanvas');
 const context = canvas.getContext('2d');
+const colors = document.querySelectorAll('.jsColor');
+const range = document.querySelector('#jsRange');
+const mode = document.querySelector('#jsMode');
+
+
+const INITIAL_COLOR = '#2c2c2c';
+
 
 let painting = false;
+let filling = false;
 
-function init(){
-    context.strokeStyle="#2c2c2c";
-    context.lineWidth=2.7;
-}
+function init() {
+    context.strokeStyle = INITIAL_COLOR;
+    context.fillStyle=INITIAL_COLOR;
+    context.lineWidth = 2.7;
+    canvas.width = 700;
+    canvas.height = 700;
 
-function startPaint(event){
-    painting=true;
-    context.beginPath();
-    // context.moveTo(0,0);
-    context.lineTo(100,100);
-    context.stroke();
-}
-
-function stopPaint(){
-    painting=false;
-    context.closePath();
-}
-function onMouseMove(event){
-    let x=event.offsetX+canvas.offsetLeft;
-    let y=event.offsetY+canvas.offsetTop;
-
-    if(painting){
-        context.lineTo(x,y);
-        context.stroke();
+    if(canvas) {
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("mousedown", startPaint);
+        canvas.addEventListener("mouseup", stopPaint);
+        canvas.addEventListener("mouseleave", stopPaint);
+    }
+    
+    if (colors) {
+        Array
+            .from(colors)
+            .forEach(element => element.addEventListener("click", handleColorClick));
+    }
+    if (range) {
+        range.addEventListener("input", handleRangeClick);
+    }
+    if (mode) {
+        mode.addEventListener("click", handleModeClick);
     }
 }
-if(canvas){
-    init();
-    canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mousedown",startPaint);
-    canvas.addEventListener("mouseup",stopPaint);
-    canvas.addEventListener("mouseleave",stopPaint);
-    
+
+function startPaint() {
+    painting = true;
+
+    if (filling) {
+        context.fillRect(0, 0, canvas.width, canvas.height);
+    }
+
 }
+
+function stopPaint() {
+    painting = false;
+    context.closePath();
+}
+function onMouseMove(event) {
+    let x = event.offsetX;
+    let y = event.offsetY;
+
+    if (painting && !filling) {
+        context.lineTo(x, y);
+        context.stroke();
+    } else {
+        context.beginPath();
+        context.moveTo(x, y);
+    }
+}
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    context.strokeStyle = color;
+    context.fillStyle=color;
+}
+function handleRangeClick(event) {
+    // const size = range.value;
+    const size = event.target.value;
+    context.lineWidth = size;
+}
+
+function handleModeClick(event) {
+    const text = event.target.innerText;
+    if (text === 'FILL') {
+        event.target.innerText = 'PAINT'
+        filling = true;
+    } else {
+        event.target.innerText = 'FILL'
+        filling = false;
+    }
+}
+init();
